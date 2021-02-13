61934fc8fc631960b64a696000a2247c2b37597d
Rate980 <xv0nnj0@icloud.com>
2021-02-13 18:55:55 +0900

    test

---
+++ controller.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
